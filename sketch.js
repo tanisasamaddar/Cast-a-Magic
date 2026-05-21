c4d32ccf15754f01ca36e21cfeff997fb57835c7
Tanisa Samaddar <48171018+tanisasamaddar@users.noreply.github.com>
2026-05-20 21:10:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let stars = [];
 let colors = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth-1, windowHeight-1);
   angleMode(DEGREES);
   background(0, 0);
   colors = ["#C59FC9", "#d90368", "#f15156", "#f4d58d"];
@@ -31,5 +31,5 @@ function mouseDragged() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth-1, windowHeight-1);
 }
